84c772fed3e8e5cae6945e02e979143e7a65927e
sravanisesi <sravani.k@dhatvibs.com>
2025-09-19 17:40:09 +0530

    commited all files

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MenuScreen from './screens/MenuScreen';
+import FoodDetailScreen from './screens/FoodDetailScreen'; // Renamed from food.js
+import YourOrderScreen from './screens/YourOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
+import OrderSummary from './screens/OrderSummary';
+import PaymentScreen from './screens/PaymentScreen';
+import HomeScreen from './screens/HomeScreen';
+import SignUpScreen from './screens/SignUp';
+import SignInScreen from './screens/SignIn';
+import StartPage from './screens/StartPage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="StartPage" component = {StartPage}/>
+        <Stack.Screen name="SignIn" component={SignInScreen}/>
+        <Stack.Screen name="SignUp" component = {SignUpScreen}/>
+        <Stack.Screen name= "HomeScreen" component = {HomeScreen}/>
+        <Stack.Screen name="MenuScreen" component={MenuScreen} />
+        <Stack.Screen name="FoodDetailScreen" component={FoodDetailScreen} />
+        <Stack.Screen name="YourOrderScreen" component={YourOrderScreen} />
+        <Stack.Screen name= "DeliveryScreen" component={DeliveryScreen}/>
+        <Stack.Screen name= "OrderSummary" component = {OrderSummary}/>
+        <Stack.Screen name="PaymentScreen" component = {PaymentScreen}/>
+        
+        
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
